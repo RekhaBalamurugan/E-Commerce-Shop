f6fd72757bd6056d7222edda0f2cb2a21360cd4d
Mownikaa <a.mownika@gmail.com>
2022-11-15 14:26:18 +0100

    Registration Functionality

--- ishop/store/models.py
+++ ishop/store/models.py
@@ -1,3 +1,16 @@
+# Create your models here.
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfileInfo(models.Model):
+    
+    user = models.OneToOneField(User)
+
+    first_name = models.Field(blank=True)
+    last_name = models.Field(blank=True)
+
+    def __str__(self):
+        return self.user.username
+        
+
