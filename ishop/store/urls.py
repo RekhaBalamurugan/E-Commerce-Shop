064ac1fd76d425586d115424ff23b662e66ee20e
jbilander <jorgen.bilander@gmail.com>
2022-11-22 18:29:26 +0100

    implemented create order functionality

--- ishop/store/urls.py
+++ ishop/store/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [
     url(r'^add_item_to_cart/$', views.add_item_to_cart, name="add_item_to_cart"),
     url(r'^update_shoppingcart/$', views.update_shoppingcart, name="update_shoppingcart"),
     url(r'^checkout/$', views.checkout, name="checkout"),
+    url(r'^order/$', views.place_order, name="order"),
     url('productdetails/(?P<id>[0-9]+)/$',views.productdetails,name="productdetails"),
 ] 
