0f18271ec76d58452f05ad161b6b3b81565d6e77
RekhaBalamurugan <rekha.bala@outlook.com>
2022-11-24 17:33:47 +0100

    added new searchbar,registered admin tables & alignment

--- ishop/store/admin.py
+++ ishop/store/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from store.models import Product,Category
+from store.models import Product,Category,Inventory,CartItem,OrderDetail,Order,Customer
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Category)
+admin.site.register(Inventory)
+admin.site.register(CartItem)
+admin.site.register(OrderDetail)
+admin.site.register(Order)
+admin.site.register(Customer)
+
 
 
 
